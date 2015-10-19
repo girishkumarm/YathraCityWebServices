0ef133f9abf370e67306f6788e7ee714ee7911c3
example <example@gmail.com>
2015-10-19 19:15:22 +0530

    commit

---
+++ modules/cassandra/com/yathraCity/cassandra/tables/CarDetailsColoumns.java
@@ -0,0 +1,16 @@
+package com.yathraCity.cassandra.tables;
+
+
+public class CarDetailsColoumns
+{
+	public static final String CAR_NAME="car_name";
+	public static final String CAR_MODEL="car_model";
+	public static final String CAR_NUMBER="car_number";
+	public static final String MAXIMUM_PEOPLE="maximum_peoples";
+	public static final String DRIVE_NAME="driver_name";
+	public static final String PHONE_NUMBER="phone_number";
+	public static final String CITY_NAME="city_name";
+	public static final String MINIMUM_DISTANCE="minimum_distance";
+	public static final String PRICE_PERKM="minimum_price_per_km";
+	public static final String DRIVER_LICENCE="driver_licence_no"; 
+}
---
+++ modules/domain/com/yathraCity/services/CountingService.java
@@ -0,0 +1,63 @@
+package com.yathraCity.services;
+import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.ws.WebServiceContext;
+import javax.ws.rs.core.StreamingOutput;
+import javax.xml.ws.handler.MessageContext;
+import javax.ws.rs.WebApplicationException;
+import com.razorthink.runtime.*;
+import defaultpkg.*;
+@Path("/counting")
+public class CountingService{
+	@Context
+	HttpServletRequest request;
+
+
+	@Context
+	HttpServletResponse response;
+
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/count-number-users")
+	public com.yathraCity.core.Message countNumberOfUsers(){
+		com.yathraCity.core.Message resp=null;
+		SessionUtils.clear(request);
+		ServiceExecutionContext context=null;
+		try
+		{
+			context = new ServiceExecutionContext(request);
+			CountingServiceInterface intfc =  (CountingServiceInterface) (ServiceParser.getImpl("com.yathraCity.services.CountingService"));
+			if (intfc == null) throw new ExecException(ErrorCodes.APPLICATION_ERROR,null,"Service not implemented");
+			resp =  intfc.countNumberOfUsers(context);
+			SessionUtils.clear(request);
+			return resp;
+		} catch (ExecException ee)
+		{
+			ee.printStackTrace(); // needs to be logged
+			request.setAttribute("WS_ERROR_MSG", ee.toJson());
+			throw ee;
+		} catch (Exception e)
+		{
+			e.printStackTrace(); // needs to be logged
+			ExecException ee = new ExecException(ErrorCodes.APPLICATION_ERROR,e,"Application Error");
+			request.setAttribute("WS_ERROR_MSG", ee.toJson());
+			throw ee;
+		}
+	}
+
+
+}
