6ad8e9cee756d2d7769d17e9b8e5bdda75dfc3eb
girish-kumar-710 <girish.kumar@razorthink.net>
2016-01-05 09:19:16 +0530

    removing unwanted files

--- modules/cassandra/com/yathraCity/cassandra/tables/EnterCoupensInterface.java
+++
@@ -1,8 +0,0 @@
-package com.yathraCity.cassandra.tables;
-import com.razorthink.runtime.*;
-import javax.ws.rs.core.StreamingOutput;
-public interface EnterCoupensInterface{
-	public com.yathraCity.core.RegisterCarResponse coupens(ServiceExecutionContext ctx,com.yathraCity.core.CoupenDeails coupensDetails) throws ExecException;
-
-
-}
--- modules/cassandra/com/yathraCity/cassandra/tables/ListOfCoupensInTabInterface.java
+++
@@ -1,8 +0,0 @@
-package com.yathraCity.cassandra.tables;
-import com.razorthink.runtime.*;
-import javax.ws.rs.core.StreamingOutput;
-public interface ListOfCoupensInTabInterface{
-	public com.yathraCity.core.ListOfCoupensUsed getallthecupons(ServiceExecutionContext ctx) throws ExecException;
-
-
-}
