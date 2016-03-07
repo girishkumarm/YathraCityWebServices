96ab6f1b4090d44efd2b478d4cad3b83afee3d9e
ashwingadam1 <ashwin.yathracity@gmail.com>
2016-03-08 01:38:39 +0530

    commit
    fixed the isssue of getting the count from the counter table

--- modules/cassandra/com/yathraCity/cassandra/dao/CountingUsersDAO.java
+++ modules/cassandra/com/yathraCity/cassandra/dao/CountingUsersDAO.java
@@ -15,54 +15,75 @@ import com.yathraCity.core.TotalNumberOfUsers;
 import com.yathraCity.services.config.ConfigKey;
 import com.yathraCity.services.config.Configurator;
 
-public class CountingUsersDAO 
-{
+public class CountingUsersDAO {
 
-	private static Logger logger = LoggerFactory.getLogger( UserDAO.class );
+	private static Logger logger = LoggerFactory.getLogger(CountingUsersDAO.class);
 	private static CassandraQuery cassQuery = null;
 	private String keyspace;
 
 	public CountingUsersDAO()
 	{
-		
+
 		try
 		{
-			cassQuery=new CassandraQuery();
-			keyspace=Configurator.getInstance().getProperty(ConfigKey.CASSANDRA_Keyspace);
+			cassQuery = new CassandraQuery();
+			keyspace = Configurator.getInstance().getProperty(ConfigKey.CASSANDRA_Keyspace);
 		}
-		catch( Exception  e )
+		catch( Exception e )
 		{
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error("error while creating the object"+e.getMessage());
 		}
 	}
-	
+
 	public void countUsers() throws Exception
 	{
-		Statement update=QueryBuilder.update(keyspace,TableNames.COUNT_VIEWS )
-				.with(QueryBuilder.incr(NumberOfUsers.COUNTER_VALUE))
-				.where(QueryBuilder.eq(NumberOfUsers.URL_NAME,"tabcars.in"));
-		cassQuery.executeFuture( update );
+		try
+		{
+			Statement update = QueryBuilder.update(keyspace, TableNames.COUNT_VIEWS)
+					.with(QueryBuilder.incr(NumberOfUsers.COUNTER_VALUE))
+					.where(QueryBuilder.eq(NumberOfUsers.URL_NAME, "tabcars.in"));
+			cassQuery.executeFuture(update);
+		}
+		catch( Exception e )
+		{
+			logger.error("error while incrementing the users viewing the website"+e.getMessage());
+		}
 	}
 
-    public int getNumberOfUsers() throws Exception
-    {
-    	Statement get=QueryBuilder.select().from(null, TableNames.COUNT_VIEWS)
-    			.where(QueryBuilder.eq(NumberOfUsers.URL_NAME, "tabcars.in"));
-    	ResultSetFuture result=cassQuery.executeFuture(get);
-    	List<TotalNumberOfUsers> users=process(result);
-    	return users.get(1).getCount();
-    }
-    
-    private List<TotalNumberOfUsers> process(ResultSetFuture result)
-    {
-    	List<TotalNumberOfUsers> users=new ArrayList<TotalNumberOfUsers>();
-    	for( Row r: result.getUninterruptibly())
-    	{
-    		TotalNumberOfUsers totalCount=new TotalNumberOfUsers();
-    		totalCount.setCount(Integer.valueOf(r.getString(NumberOfUsers.COUNTER_VALUE)));
-    		users.add(totalCount);
-    	}
-    	return users;
-    }
+	public int getNumberOfUsers() throws Exception
+	{
+		List<TotalNumberOfUsers> users = null;
+		try
+		{
+			Statement get = QueryBuilder.select().from(null, TableNames.COUNT_VIEWS)
+					.where(QueryBuilder.eq(NumberOfUsers.URL_NAME, "tabcars.in"));
+			ResultSetFuture result = cassQuery.executeFuture(get);
+			users = process(result);
+		}
+		catch( Exception e )
+		{
+			logger.error("error while getting the number of userve  from DB"+e.getMessage());
+		}
+		return users.get(0).getCount();
+	}
+
+	private List<TotalNumberOfUsers> process( ResultSetFuture result )
+	{
+		List<TotalNumberOfUsers> users = new ArrayList<TotalNumberOfUsers>();
+		try
+		{
+			for( Row r : result.getUninterruptibly() )
+			{
+				TotalNumberOfUsers totalCount = new TotalNumberOfUsers();
+				totalCount.setCount((int) (r.getLong(NumberOfUsers.COUNTER_VALUE)));
+				users.add(totalCount);
+			}
+		}
+		catch( Exception e )
+		{
+			logger.error("error while getting the details of the count from the DB in process method"+e.getMessage());
+		}
+		return users;
+	}
 }
